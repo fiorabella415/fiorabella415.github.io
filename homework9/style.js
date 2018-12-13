be8ada7779260783254850af706ff5d33e541d12
Fiorabella <fkc221@nyu.edu>
2018-12-13 16:44:45 -0500

    Update style.js

--- homework9/style.js
+++ homework9/style.js
@@ -1,7 +1,7 @@
 function HTMLElement() {
-    var divNum = document.getElementsByTagName('div').length;
-    var pNum = document.getElementsByTagName('p').length;
-    var h1Num = document.getElementsByTagName('h1').length;
+    var divNumber = document.getElementsByTagName('div').length;
+    var pNumber = document.getElementsByTagName('p').length;
+    var h1Number = document.getElementsByTagName('h1').length;
 
     alert("There are " +
         divNum + " div elements, " +
